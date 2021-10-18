c1b8c109c493f297986c677d5dbeea23c252482b
narsispecial <narsispecial@gmail.com>
2021-10-18 22:26:29 +0800

    first commit

---
+++ src/main/java/in/nareshit/raghu/controller/SpecializationController.java
@@ -0,0 +1,149 @@
+package in.nareshit.raghu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import in.nareshit.raghu.entity.Specialization;
+import in.nareshit.raghu.service.ISpecializationService;
+import in.nareshit.raghu.service.exception.SpecializationNotFoundException;
+import in.nareshit.rahu.view.SpecializationExcelView;
+
+@Controller
+@RequestMapping("/spec")
+public class SpecializationController {
+
+	@Autowired
+	private ISpecializationService service;
+	
+	/***
+	 * 1. Show Register page
+	 */
+	@GetMapping("/register")
+	public String displayRegister() {
+		return "SpecializationRegister";
+	}
+	
+	/**
+	 * 2. On Submit Form save data
+	 */
+	@PostMapping("/save")
+	public String saveForm(
+			@ModelAttribute Specialization specialization,
+			Model model
+			)
+	{
+		Long id = service.saveSpecialization(specialization);
+		String message ="Record ("+id+") is created";
+		model.addAttribute("message", message);
+		return "SpecializationRegister";
+	}
+	
+	/**
+	 * 3. display all Specializations
+	 */
+	@GetMapping("/all")
+	public String viewAll(
+			Model model,
+			@RequestParam(value = "message",required = false) String message
+			)
+	{
+		List<Specialization> list = service.getAllSpecializations();
+		model.addAttribute("list",list);
+		model.addAttribute("message", message);
+		return "SpecializationData";
+	}
+	
+	/**
+	 * 4. Delete by id
+	 */
+	@GetMapping("/delete")
+	public String deleteData(
+			@RequestParam Long id,
+			RedirectAttributes attributes
+			) 
+	{
+		try{
+			service.removeSpecialization(id);
+			attributes.addAttribute("message", "Record ("+id+") is removed");
+			
+		}catch(SpecializationNotFoundException e) {
+			e.printStackTrace();
+			attributes.addAttribute("message", e.getMessage());
+		}
+		
+		return "redirect:all";
+	}
+	
+	/**
+	 * 5. Fetch Data into Edit page
+	 */
+	@GetMapping("/edit")
+	public String showEditPage(
+			@RequestParam Long id,
+			Model model
+			) 
+	{
+		Specialization spec = service.getOneSpecialization(id);
+		model.addAttribute("specialization", spec);
+		return "SpecializationEdit";
+	}
+	
+	/***
+	 * 6. Update Form data and redirect to all
+	 */
+	@PostMapping("/update")
+	public String updateData(
+			@ModelAttribute Specialization specialization,
+			RedirectAttributes attributes
+			)
+	{
+		service.updateSpecialization(specialization);
+		attributes.addAttribute("message", "Record ("+specialization.getId()+") is updated");
+		return "redirect:all";
+	}
+	
+	/**
+	 * 7. Read code and check with service
+	 *    Return message back to UI 
+	 */
+	@GetMapping("/checkCode")
+	@ResponseBody
+	public String validateSpecCode(
+			@RequestParam String code
+			) 
+	{
+		String message = "";
+		if(service.isSpecCodeExist(code)) {
+			message = code + ", already exist";
+		} 
+		
+		return message; //this is not viewName(it is message)
+	}
+	
+	
+	@GetMapping("/excel")
+	public ModelAndView exportToExcel() {
+		ModelAndView m =  new ModelAndView();
+		m.setView(new SpecializationExcelView());
+		
+		//read data from DB
+		List<Specialization> list = service.getAllSpecializations();
+		//send to Excel Impl class
+		m.addObject("list", list);
+		
+		return m;
+	}
+	
+	
+}
